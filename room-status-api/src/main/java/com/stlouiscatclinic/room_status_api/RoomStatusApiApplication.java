88b3629c009c8b0b59babd885f46b78d7a2bad82
Trevor Gruber <gruber.trevor9281@gmail.com>
2024-06-27 23:49:58 -0500

    ui and api projects created

---
+++ room-status-api/src/main/java/com/stlouiscatclinic/room_status_api/RoomStatusApiApplication.java
@@ -0,0 +1,13 @@
+package com.stlouiscatclinic.room_status_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomStatusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomStatusApiApplication.class, args);
+	}
+
+}
